06f3c7a44e0a9d49131a7dd4a8e171af6f3e46b6
Marc Guay <marc.guay@gmail.com>
2023-02-26 14:04:01 -0500

    Tidy

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -8,7 +8,6 @@ const Content = (props) => (
     <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
     <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
   </div>
-  
 )
 
 const Part = (props) => (
