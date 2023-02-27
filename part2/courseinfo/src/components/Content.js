08ac0cc59766c349d16edbc25674ff957220e780
Marc Guay <marc.guay@gmail.com>
2023-02-27 15:06:15 -0500

    2.4: Course information step9

--- part2/courseinfo/src/components/Content.js
+++ part2/courseinfo/src/components/Content.js
@@ -1,11 +1,20 @@
 import Part from './Part'
 
-const Content = ({ parts }) => (
+const Content = ({ parts }) => {
+
+  const initialValue = 0;
+  const total = parts.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue.exercises
+  }, initialValue)
+
+  return (
   <div>
     {parts.map(part => 
-        <Part key={part.id} part={part.name} exercises={part.exercises} />
+      <Part key={part.id} part={part.name} exercises={part.exercises} />
     )}
+    <strong>total of {total} exercises</strong>
   </div>
-)
+  )
+}
 
 export default Content
