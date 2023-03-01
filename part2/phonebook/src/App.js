a60e23e911b96a0c03942eae2675a1e27f3c8237
Marc Guay <marc.guay@gmail.com>
2023-03-01 14:44:02 -0500

    Finish 2.10: The Phonebook Step5

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,40 +1,78 @@
 import { useState } from 'react'
 
+const Filter = ({ value, onChange }) => (
+  <div>
+    <span>filter shown with</span>
+    <input value={value} onChange={onChange} />
+  </div>
+)
+
+const PersonForm = ({ onSubmit, nameVal, nameChange, numVal, numChange }) => (
+  <form onSubmit={onSubmit}>
+    <div>
+      name:
+      <input value={nameVal} onChange={nameChange} />
+    </div>
+    <div>number: 
+      <input value={numVal} onChange={numChange} />
+    </div>
+    <div>
+      <button type="submit">add</button>
+    </div>
+  </form>
+)
+
+const Persons = ({ persons }) => (
+  persons.map(person =>
+    <div key={person.name}>{person.name} {person.number}</div>
+  )
+)
+
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456'},
+    { name: 'Ada Lovelace', number: '39-44-5323523'},
+    { name: 'Dan Abramov', number: '12-43-234345'},
+    { name: 'Mary Poppendieck', number: '39-23-6423122'}
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [searchFilter, setSearchFilter] = useState('')
+  
+  const peopleToShow = persons.filter(person => person.name.toLowerCase().includes(searchFilter.toLowerCase()))
 
   const addName = (event) => {
     event.preventDefault();
-    setPersons(persons.concat({name : newName}))
-    setNewName('')
+
+    if (persons.find(person => person.name == newName)){
+      alert(`${newName} already exists in the phonebook`)
+    } else {
+      setPersons(persons.concat({name: newName, number: newNumber}))
+      setNewName('')
+      setNewNumber('')
+    }
   }
   
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
   
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+  
+  const handleSearchFilterChange = (event) => {
+    setSearchFilter(event.target.value)
+  }
+  
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addName}>
-        <div>
-          name:
-          <input 
-            value={newName}
-            onChange={handleNameChange}
-          />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      {persons.map(person => 
-        <div key={person.name}>{person.name}</div>
-      )}
+      <Filter value={searchFilter} onChange={handleSearchFilterChange} />
+      <h3>Add a new</h3>
+      <PersonForm onSubmit={addName} nameVal={newName} nameChange={handleNameChange} numVal={newNumber} numChange={handleNumberChange} />
+      <h3>Numbers</h3>
+      <Persons persons={peopleToShow} />
     </div>
   )
 }
