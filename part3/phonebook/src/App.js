ac45c5d17fb79d34a168474ae79ef2434647f27f
Marc Guay <marc.guay@gmail.com>
2023-04-02 11:12:34 -0400

    eslint fix

--- part3/phonebook/src/App.js
+++ part3/phonebook/src/App.js
@@ -63,7 +63,7 @@ const App = () => {
   const addName = (event) => {
     event.preventDefault()
     const newPerson = {name: newName, number: newNumber}
-    const existingPerson = persons.find(person => person.name == newName)
+    const existingPerson = persons.find(person => person.name === newName)
 
     if (existingPerson){
       if (window.confirm(`${existingPerson.name} is already added to the phonebook, replace the old number with a new one?`)){
