98409ba5cfd15a50045dc8c7fc864cb5c0609437
Marc Guay <marc.guay@gmail.com>
2023-04-08 18:13:38 -0400

    mostBlogs

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -18,8 +18,31 @@ const favouriteBlog = (blogs) => {
   return blogs.reduce(reducer, 0);
 }
 
+const mostBlogs = (blogs) => {
+  
+  // Create an array of objects that hold the author name and their total blog count
+  let counter = []
+  for (let blog of blogs) {
+    const index = counter.findIndex(entry => entry.author===blog.author);
+    if (index > 0){
+      // If the author is already in the counter array, update the blogs count
+      counter[index].blogs++
+    } else {
+      // Add the author to the counter array
+      counter.push({'author':blog.author, 'blogs':1})
+    }
+  }
+  
+  // Loop and filter for the author with the most blogs
+  // return an object with author name and blog count
+  return counter.reduce((acc, cur) => {
+    return cur.blogs > acc.blogs ? cur : acc
+  }, {'author':undefined, 'blogs':0});
+}
+
 module.exports = {
   dummy,
   totalLikes,
-  favouriteBlog
+  favouriteBlog,
+  mostBlogs
 }
