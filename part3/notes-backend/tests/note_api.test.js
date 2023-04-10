46230b31dd35f75a3b3e11fb5334713cd3e3480c
Marc Guay <marc.guay@gmail.com>
2023-04-10 11:00:28 -0400

    Refactoring beforeEach to use async

--- part3/notes-backend/tests/note_api.test.js
+++ part3/notes-backend/tests/note_api.test.js
@@ -5,23 +5,13 @@ const app = require('../app')
 const api = supertest(app)
 const Note = require('../models/note')
 
-const initialNotes = [
-  {
-    content: 'HTML is easy',
-    important: false,
-  },
-  {
-    content: 'Browser can execute only JavaScript',
-    important: true,
-  },
-]
-
 beforeEach(async () => {
   await Note.deleteMany({})
-  let noteObject = new Note(helper.initialNotes[0])
-  await noteObject.save()
-  noteObject = new Note(helper.initialNotes[1])
-  await noteObject.save()
+
+  for (let note of helper.initialNotes) {
+    let noteObject = new Note(note)
+    await noteObject.save()
+  }
 })
 
 test('notes are returned as json', async () => {
