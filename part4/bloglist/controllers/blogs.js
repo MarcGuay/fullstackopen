9b58f82682c492fe7af207705cec10589edbba85
Marc Guay <marc.guay@gmail.com>
2023-04-10 12:42:55 -0400

    Add controller and test for adding blog post

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -6,4 +6,10 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs)
 })
 
+blogsRouter.post('/', async (request, response) => {
+  const blog = new Blog(request.body)
+  const savedBlog = await blog.save()
+  response.status(201).json(savedBlog)
+})
+
 module.exports = blogsRouter
